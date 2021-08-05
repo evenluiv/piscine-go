0de288c2ea3b8937b113ceb239d3b2b13238ab71
evenluiv <evenluiv@gmail.com>
2021-08-05 20:41:35 +0300

    makerange

--- makerange.go
+++ makerange.go
@@ -5,11 +5,11 @@ func MakeRange(min, max int) []int {
 		return nil
 	}
 
-	answer := make([]int, max)
-	i := min
+	answer := make([]int, max-min)
+	i := 0
 
-	for i < max {
-		answer[i] = i
+	for i < max-min {
+		answer[i] = i + min
 		i++
 	}
 	return answer
