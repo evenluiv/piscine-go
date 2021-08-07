8cc1249198bf678ba60a041435690cb597784f92
evenluiv <evenluiv@gmail.com>
2021-08-07 22:53:24 +0300

    asd123

--- printwordstables.go
+++ printwordstables.go
@@ -4,7 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
 	for _, b := range a {
-		SplitWhiteSpaces(b)
+		for _, c := range b {
+			z01.PrintRune(c)
+		}
 		z01.PrintRune('\n')
 	}
 }
