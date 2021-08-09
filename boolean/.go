b85b48236ca05b6f977d1d9827413c845b94c77d
evenluiv <evenluiv@gmail.com>
2021-08-09 10:58:56 +0300

    boolean

---
+++ boolean/.go
@@ -0,0 +1,22 @@
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) boolean {
+	if even(nbr) == 2 {
+		return yes
+	} else {
+		return no
+	}
+}
+
+func main() {
+	if isEven(lengthOfArg) == 2 {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
