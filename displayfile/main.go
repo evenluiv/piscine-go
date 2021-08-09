079e4c633ea47905dadebec97ea6e584d1d96a41
evenluiv <evenluiv@gmail.com>
2021-08-09 19:45:41 +0300

    asd123

--- displayfile/main.go
+++ displayfile/main.go
@@ -10,9 +10,9 @@ func main() {
 	arguments := os.Args[1:]
 	length := len(os.Args)
 	if length == 1 {
-		fmt.Print("File name missing")
+		fmt.Print("File name missing\n")
 	} else if length < 2 {
-		fmt.Print("Too many arguments")
+		fmt.Print("Too many arguments\n")
 	} else if arguments[0] == "quest8.txt" {
 
 		content, err := ioutil.ReadFile(arguments[0])
