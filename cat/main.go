1ca797ac8355c9e21412d0c6398e988c72471380
evenluiv <evenluiv@gmail.com>
2021-08-11 12:57:55 +0300

    cat

---
+++ cat/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if 0 == len(args) {
+		fmt.Print()
+	} else {
+		for _, s := range os.Args[1:] {
+			file, err := os.Open(s)
+			if err != nil {
+				fmt.Println(err.Error())
+				break
+			} else {
+				data, err := ioutil.ReadAll(file)
+				if err != nil {
+					fmt.Println(err.Error())
+					break
+				} else {
+					fmt.Printf("%s", data)
+				}
+			}
+		}
+	}
+}
